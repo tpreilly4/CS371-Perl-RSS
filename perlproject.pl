9cefb6720d0fabaec51124ad3b7e367e1d4dc26c
Luke Tomkus <luke@pkillboredom.com>
2017-02-21 10:44:07 -0500

    successfully extracts titles and urls

--- perlproject.pl
+++ perlproject.pl
@@ -1,2 +1,21 @@
 #This script will generate valid RSS XML from news.ycombinator.com
-#TODO: all.
+#Luke Tomkus and Thomas Reilly
+
+use WWW::Mechanize;
+my $mech = WWW::Mechanize->new();
+my $url = 'http://news.ycombinator.com';
+$mech->get($url);
+
+my $content = $mech->content();
+
+my @titles = $content =~ /(?<=class="storylink">)([^<]+)/g;
+my @urls = $content =~ /(?=[^"]+" class="storylink">)([^"]*)/g;
+
+
+#TESTING
+foreach my $title ( @titles ){
+	print $title , "\n";
+}
+foreach my $url ( @urls ){
+	print $url , "\n";
+}
